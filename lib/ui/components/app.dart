ae563ccb2aa031145b6b6b4030e751190016da5b
Alvez <joaor4e@gmail.com>
2023-04-11 20:45:00 -0300

    set status bar to light mode

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../pages/pages.dart';
 
 class App extends StatelessWidget {
@@ -6,9 +7,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
-    final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
-    final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    const primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    const primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
 
     return MaterialApp(
       title: "forDevs",
@@ -18,14 +20,13 @@ class App extends StatelessWidget {
           primaryColor: primaryColor,
           primaryColorDark: primaryColorDark,
           primaryColorLight: primaryColorLight,
-          accentColor: primaryColor,
           focusColor: primaryColor,
-          textTheme: TextTheme(
+          textTheme: const TextTheme(
               headlineLarge: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
                   color: primaryColorDark)),
-          inputDecorationTheme: InputDecorationTheme(
+          inputDecorationTheme: const InputDecorationTheme(
               floatingLabelStyle: TextStyle(color: primaryColor),
               enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(color: primaryColorLight)),
@@ -34,7 +35,7 @@ class App extends StatelessWidget {
               iconColor: primaryColorLight,
               alignLabelWithHint: true),
           buttonTheme: ButtonThemeData(
-              colorScheme: ColorScheme.light(primary: primaryColor),
+              colorScheme: const ColorScheme.light(primary: primaryColor),
               buttonColor: primaryColor,
               splashColor: primaryColorLight,
               textTheme: ButtonTextTheme.primary,
@@ -48,7 +49,9 @@ class App extends StatelessWidget {
                       borderRadius: BorderRadius.circular(20)),
                   backgroundColor: primaryColor,
                   surfaceTintColor: primaryColorLight,
-                  textStyle: Theme.of(context).textTheme.bodyMedium))),
+                  textStyle: Theme.of(context).textTheme.bodyMedium)),
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: primaryColor)),
       home: const LoginPage(),
     );
   }
